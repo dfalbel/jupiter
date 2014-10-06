5342bc8a9f3bec0a5782e65f1488e2dd6bf37a77
Daniel Falbel <dfalbel@gmail.com>
2014-10-06 14:59:49 -0400

    small changes

--- R/jupiter.R
+++ R/jupiter.R
@@ -1,5 +1,8 @@
-jup_ue <- function(){
-  ue <- "https://uspdigital.usp.br/jupiterweb/jupColegiadoLista?tipo=D" %>% GET() %>% html() %>% html_node("table")
+jup_unidades_ensino <- function(){
+  
+  
+  
+  ue <- "https://uspdigital.usp.br/jupiterweb/jupColegiadoLista?tipo=D" %>% html() %>% html_node("table")
   ue.tab <- ue[[3]] %>% html_table(header = T)
   names(ue.tab)[1] <- "Código"
   return(ue.tab)
