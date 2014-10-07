98962a4bd7dfbb9c6bb6c76a50eae19c54853e24
Daniel Falbel <dfalbel@gmail.com>
2014-10-07 11:07:50 -0400

    funçaõ que devolve as unidades de ensino

--- R/jupiter.R
+++ R/jupiter.R
@@ -1,9 +1,12 @@
+#' unidades de ensino da usp
+#' 
+#' @description devolve um data.frame com o código e o nome de todas as unidades de ensino da usp.
 jup_unidades_ensino <- function(){
   
-  
-  
-  ue <- "https://uspdigital.usp.br/jupiterweb/jupColegiadoLista?tipo=D" %>% html() %>% html_node("table")
-  ue.tab <- ue[[3]] %>% html_table(header = T)
-  names(ue.tab)[1] <- "Código"
-  return(ue.tab)
+  uni.ens <- rvest::html("https://uspdigital.usp.br/jupiterweb/jupColegiadoLista?tipo=D")
+  uni.ens  <- rvest::html_nodes(uni.ens,"table")
+  uni.ens_tab <- rvest::html_table(uni.ens[[3]],header = T)
+  uni.ens_tab <- fix_encoding_df(uni.ens_tab)
+  return(uni.ens_tab)
 }
+
